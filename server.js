a1ff85267f080fafb1e52b354cf053fe0fe78a03
Alexandre Frigon <alexandre.frigon.1@gmail.com>
2019-04-23 01:46:44 -0400

    removed fs deps

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 const server = require('http').createServer(app)
-const fs = require('fs')
 const path = require('path')
 let data = require("./data.json")
 
